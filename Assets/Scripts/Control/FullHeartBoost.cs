8f0a6cf912d30a38047eecec9c59fb53e2eb0369
Franco Pistasoli <77682687+fpistasoli@users.noreply.github.com>
2022-05-16 11:48:21 +0200

    Added full health tap implementation closes #48

--- Assets/Scripts/Control/FullHeartBoost.cs
+++ Assets/Scripts/Control/FullHeartBoost.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,24 +7,48 @@ namespace VeggieNightmare.Control
 {
     public class FullHeartBoost : MonoBehaviour
     {
+        [SerializeField] private float cameraViewOffset = 0.1f;
+        [SerializeField] private float speed;
 
-
+        private Camera mainCamera;
+        private Vector3 fullHeartBoostVelocity;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            mainCamera = Camera.main;
+            fullHeartBoostVelocity = Vector3.left * speed;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (!IsViewableInCamera()) return;
+            else
+            {
+                Move();
+            }
 
-            //DestroyIfOutOfSight()
+            DestroyIfOutOfSight();
 
         }
 
+        private void Move()
+        {
+            transform.Translate(fullHeartBoostVelocity * Time.deltaTime);
+        }
 
+        private void DestroyIfOutOfSight()
+        {
+            Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+            if (viewportPosition.x < -0.1f) { Destroy(gameObject); }
+        }
+
+        public bool IsViewableInCamera()
+        {
+            Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+            return viewportPosition.x <= 1.0f + cameraViewOffset;
+        }
 
 
     }
